86c9aea316e7520a3b3e689a2dd9acdb58c94812
Rodrigo Barros <brodrigo87@gmail.com>
2026-01-07 15:58:26 -0300

    test3

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -5,60 +5,64 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
 
-export async function POST(req: Request) {
-  try {
-    const { email, password } = await req.json()
-    const JWT_SECRET = process.env.JWT_SECRET!
-
-    if (!JWT_SECRET) {
-      throw new Error('JWT_SECRET not defined')
-    }
-    if (!email || !password) {
-      return NextResponse.json(
-        { message: 'Missing fields' },
-        { status: 400 }
-      )
-    }
-
-    const user = await prisma.user.findUnique({
-      where: { email },
-    })
-
-    if (!user || !user.password) {
-      return NextResponse.json(
-        { message: 'Invalid credentials' },
-        { status: 401 }
-      )
-    }
-
-    const isValid = await bcrypt.compare(password, user.password)
-
-    if (!isValid) {
-      return NextResponse.json(
-        { message: 'Invalid credentials' },
-        { status: 401 }
-      )
-    }
-
-    const token = jwt.sign(
-      { id: user.id, email: user.email },
-      JWT_SECRET,
-      { expiresIn: '7d' }
-    )
-
-    return NextResponse.json({
-      token,
-      user: {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-      },
-    })
-  } catch (error) {
-    console.error('[LOGIN_ERROR]', error)
-    return NextResponse.json(
-      { message: 'Internal server error' },
-      { status: 500 }
-    )
-  }
+export async function POST() {
+  return NextResponse.json({ ok: true })
 }
+
+// export async function POST(req: Request) {
+//   try {
+//     const { email, password } = await req.json()
+//     const JWT_SECRET = process.env.JWT_SECRET!
+
+//     if (!JWT_SECRET) {
+//       throw new Error('JWT_SECRET not defined')
+//     }
+//     if (!email || !password) {
+//       return NextResponse.json(
+//         { message: 'Missing fields' },
+//         { status: 400 }
+//       )
+//     }
+
+//     const user = await prisma.user.findUnique({
+//       where: { email },
+//     })
+
+//     if (!user || !user.password) {
+//       return NextResponse.json(
+//         { message: 'Invalid credentials' },
+//         { status: 401 }
+//       )
+//     }
+
+//     const isValid = await bcrypt.compare(password, user.password)
+
+//     if (!isValid) {
+//       return NextResponse.json(
+//         { message: 'Invalid credentials' },
+//         { status: 401 }
+//       )
+//     }
+
+//     const token = jwt.sign(
+//       { id: user.id, email: user.email },
+//       JWT_SECRET,
+//       { expiresIn: '7d' }
+//     )
+
+//     return NextResponse.json({
+//       token,
+//       user: {
+//         id: user.id,
+//         name: user.name,
+//         email: user.email,
+//       },
+//     })
+//   } catch (error) {
+//     console.error('[LOGIN_ERROR]', error)
+//     return NextResponse.json(
+//       { message: 'Internal server error' },
+//       { status: 500 }
+//     )
+//   }
+// }
